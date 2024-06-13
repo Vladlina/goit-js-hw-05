a4f84c165c5d659302d25f9faf144321b0140fca
Vladlina <92329114+Vladlina@users.noreply.github.com>
2024-06-13 15:34:46 +0200

    task 4 completion

--- js/task-4.js
+++ js/task-4.js
@@ -1,4 +1,7 @@
-//
+const getTotalBalanceByGender = (users, gender) =>
+  users
+    .filter(user => user.gender === gender)
+    .reduce((sum, user) => sum + user.balance, 0);
 
 const clients = [
   {
